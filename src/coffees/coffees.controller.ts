c777f6d6608c82f45a560004f966a9e87a2bd515
watsoncolin <colin@colinwatson.me>
2023-09-21 15:29:58 -0600

    auto transform payloads

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -24,13 +24,14 @@ export class CoffeesController {
     return this.coffeesService.findAll();
   }
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    return createCoffeeDto;
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeesService.create(createCoffeeDto);
   }
 
