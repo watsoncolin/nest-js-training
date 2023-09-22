90b30a777a8330c724170ed25419f9ebc532da8c
watsoncolin <colin@colinwatson.me>
2023-09-22 10:35:18 -0600

    add entity update service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -25,13 +25,11 @@ export class CoffeesController {
   }
   @Get(':id')
   findOne(@Param('id') id: number) {
-    console.log(typeof id);
     return this.coffeesService.findOne('' + id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeesService.create(createCoffeeDto);
   }
 
