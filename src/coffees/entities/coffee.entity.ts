95753a25d5744ceba82e7879d4f1cc98e0e66baa
watsoncolin <colin@colinwatson.me>
2023-09-22 10:45:05 -0600

    flavor entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
 
 @Entity()
 export class Coffee {
@@ -8,6 +15,7 @@ export class Coffee {
   name: string;
   @Column()
   brand: string;
-  @Column('json', { nullable: true })
+  @JoinTable()
+  @ManyToMany(() => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
