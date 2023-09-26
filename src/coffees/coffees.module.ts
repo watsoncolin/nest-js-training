b355f8f8cea0fc0e9227f54254cb61ef6336c9df
watsoncolin <colin@colinwatson.me>
2023-09-26 17:18:06 -0600

    class providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,11 +6,24 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffee.constants';
 
+class ConfigService {}
+
+class DevelopementConfigService {}
+
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   providers: [
     CoffeesService,
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopementConfigService
+          : ProductionConfigService,
+    },
   ],
   controllers: [CoffeesController],
   exports: [CoffeesService],
